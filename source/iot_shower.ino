285928dba1462568833746434fe6c2f3f48e8992
123yagel <123yagel@gmail.com>
2018-04-26 19:32:04 +0300

    Update iot_shower.ino

--- source/iot_shower.ino
+++ source/iot_shower.ino
@@ -3,14 +3,13 @@
 
 
 const int  d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+const int rs = 12, en = 11;  // for led disply pins defines
 const int btnUP = 7, btnDWN = 8;
 const int cold_valve=9,warm_valve=10;
-const int rs = 12, en = 11;  // pins defines
+
 
 Servo cold_servo;
 Servo warm_servo;
-int num = 0;
-
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -21,28 +20,53 @@ bool btnDWNclicked = false;
 float wantedTemp = 40.0;
 float currentTemp = 38;
 
-
-
-
-
-
-
 bool move_valve(Servo valve, uint16_t deg);
 void checkButtons();
-float measureTemp();//todo
+void measureTemp();
 void printWntTmp();
 void printCrntTmp();
+void FloatToString(char *str, float f, char size);
 
-
+void FloatToString(char *str, float f, char size){
+   char count;
+  char* curr;
+  int value;
+  value = (int)f;
+  itoa(value,str, 10);
+  count = 0;
+  curr = str;
+  while(*curr != 0){
+    ++count;
+    ++curr;
+  }
+ 
+  if(count + 1 >= size){
+    str[size - 1] = 0;
+    return; 
+  }
+  
+  str[count++] = '.';
+  ++curr;
+  f = f - (float)value;
+  
+  while(count + 1 < size){
+    f *= 10;
+    ++count;  
+  }
+  
+  value = (int)f;
+  itoa(value,curr, 10);
+  str[size - 1] = 0;
+  return;
+ }
 
 void setup() {
 	Serial.begin(9600);
 	pinMode(btnUP, INPUT_PULLUP);
 	pinMode(btnDWN, INPUT_PULLUP);
-
+	pinMode(tmpSensorPin, INPUT);
 
 	lcd.begin(16, 2);
-
 
 	printWntTmp();
 	printCrntTmp();
@@ -63,21 +87,26 @@ void loop() {
 
 
 
-void printWntTmp() {
-	lcd.setCursor(0, 0);
-	lcd.print(((float)((int)(wantedTemp * 10))) / 10);
-	lcd.setCursor(4, 0);
-	lcd.print((char)223);
+void printWntTmp(){
+  lcd.setCursor(0, 0);
+  char s[5];
+  FloatToString(s, wantedTemp, 5);
+  lcd.print(s);
+  lcd.setCursor(4, 0);
+  lcd.print((char)223);
 }
 
-void printCrntTmp() {
-	lcd.setCursor(0, 1);
-	lcd.print('(');
-	lcd.print(((float)((int)(currentTemp * 10))) / 10);
-	lcd.setCursor(5, 1);
-	lcd.print((char)223);
-	lcd.print(')');
+void printCrntTmp(){
+  lcd.setCursor(0, 1);
+  lcd.print('(');
+  char s[5];
+  FloatToString(s, currentTemp, 5);
+  lcd.print(s);
+  lcd.setCursor(5, 1);
+  lcd.print((char)223);
+  lcd.print(')');
 }
+
 
 void checkButtons() {
 	if (digitalRead(btnUP) == LOW) {
@@ -113,4 +142,12 @@ bool move_valve(Servo valve, uint16_t deg)//WARNING when using dont call all the
 		valve.write(deg);
 	}
 	return true;
+}
+
+void measureTemp(){
+   double val=analogRead(0);
+  double fenya=(val/1023)*5;
+  // Ohm Law r/100=fenya/(3.3-fenya)
+  double r=fenya/(3.3-fenya)*100;
+  currentTemp =  ( 1/(  log(r/10) /3000 + 1/(25+273.15)   )-273.15);
 }
